d91b5898d042590dc24f7f79a6c2ba6aea028cf5
joon <khjoon372@naver.com>
2023-01-26 14:57:34 +0900

    create foreign key

--- betterMe.sql
+++ betterMe.sql
@@ -58,5 +58,33 @@ CREATE TABLE `follow` (
 	`acceptedAt` TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP
 );
 
+--foreign key 추가
+ALTER TABLE `habit` ADD CONSTRAINT `FK_UserTBL_TO_habit_1` FOREIGN KEY (`userIdx`) REFERENCES `UserTBL` (`userIdx`);
+
+ALTER TABLE `habit_invite` ADD CONSTRAINT `FK_habit_TO_habit_invite_1` FOREIGN KEY (`habitIdx`)
+REFERENCES `habit` (`habitIdx`);
+
+ALTER TABLE `habit_invite` ADD CONSTRAINT `FK_UserTBL_TO_habit_invite_1` FOREIGN KEY (`senderIdx`)
+REFERENCES `UserTBL` (`userIdx`);
+
+ALTER TABLE `habit_invite` ADD CONSTRAINT `FK_UserTBL_TO_habit_invite_2` FOREIGN KEY (`receiverIdx`)
+REFERENCES `UserTBL` (`userIdx`);
+
+ALTER TABLE `habit_invite` ADD CONSTRAINT `FK_UserTBL_TO_habit_invite_3` FOREIGN KEY (`userIdx`)
+REFERENCES `UserTBL` (`userIdx`);
+
+ALTER TABLE `follow` ADD CONSTRAINT `FK_UserTBL_TO_follow_1` FOREIGN KEY (`follower`)
+ REFERENCES `UserTBL` (`userIdx`);
+
+ALTER TABLE `follow` ADD CONSTRAINT `FK_UserTBL_TO_follow_2` FOREIGN KEY (`followee`)
+REFERENCES `UserTBL` (`userIdx`);
+
 -- 회원정보 입력
 INSERT INTO UserTBL(email, pw, nickName, promise) VALUES ('mimi@gmail.com', 'f7151aa2440939ce606b0ea0eaa7b4b043a9b2e911fe435251b88ab33cee43ab56903628e2e66594f1a857a0dd918ef65b005f602d584b4d37752c18df72cd0b', '미미', '난 할 수 있서.'); -- pw : mimimimi
+
+--습관 데이터 입력
+INSERT INTO `habit` VALUES (1,1,'자전거','한강 다녀오기',1,0,'good',1,'g','2023-01-10 08:47:45','2023-01-10 08:47:45',1),
+                           (2,1,'책읽기','하루 30분',3,30,'good',0,'g','2023-01-10 08:48:01','2023-01-10 08:48:01',1),
+                           (3,1,'명상','하루 5분',0,25,'good',0,'m','2023-01-10 08:48:12','2023-01-10 08:48:12',0),
+                           (4,1,'시간낭비 하지 않기','스마트폰 1시간 이상 사용 X',3,30,'bad',0,'b','2023-01-10 08:49:28','2023-01-10 08:49:28',1),
+                           (5,1,'영어공부','토익',3,30,'good',0,'b','2023-01-11 05:00:02','2023-01-11 05:00:02',1);
